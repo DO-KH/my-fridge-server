082c4cbca27a440e69579874f1c6c3a39e834c21
dkh <dkh9390@naver.com>
2025-04-22 14:53:33 +0900

    쿠키 유지시간 설정

--- server.ts
+++ server.ts
@@ -40,6 +40,7 @@ app.use(
     cookie: {
       secure: process.env.NODE_ENV === "production",
       httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24, // 1일
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
     },
   })
